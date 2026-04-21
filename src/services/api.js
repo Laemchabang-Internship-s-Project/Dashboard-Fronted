211a7b79296284ba6724782ae4391381710784d7
apiwitB <nice456789123@gmail.com>
2026-04-21 14:33:48 +0700

    ui ip ac

--- src/services/api.js
+++ src/services/api.js
@@ -52,11 +52,11 @@ export const createEventSource = (path) => {
 
 // ===== Network Check =====
 export const checkNetwork = async () => {
-  return true;
-  // try {
-  //   const data = await apiGet("/api/check-network"); 
-  //   return data.isInternal; 
-  // } catch (error) {
-  //   return false;
-  // }
+  // return true;
+  try {
+    const data = await apiGet("/api/check-network"); 
+    return data.isInternal; 
+  } catch (error) {
+    return false;
+  }
 };
